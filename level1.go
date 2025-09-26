0cfed54aee4ebb226ff2058e5d4480068437084f
Shangruthan A <156189854+shangruthan@users.noreply.github.com>
2025-09-26 22:19:47 +0530

    importing assets for level 1 dinosaur

--- level1.go
+++ level1.go
@@ -1,196 +1,3 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"image"
-// 	"image/color"
-// 	"math/rand"
-
-// 	"github.com/hajimehoshi/ebiten/v2"
-// 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-// 	"github.com/hajimehoshi/ebiten/v2/inpututil"
-// )
-
-// type Powerup struct {
-// 	X, Y float64
-// 	Img  *ebiten.Image
-// }
-
-// type Bomb struct {
-// 	X, Y float64
-// 	Img  *ebiten.Image
-// }
-
-// type Level1 struct {
-// 	playerX           float64
-// 	playerY           float64
-// 	playerSpeed       float64
-// 	playerHealth      int
-// 	playerImg         *ebiten.Image
-// 	powerups          []*Powerup
-// 	powerupsCollected int
-// 	bombs             []*Bomb
-// 	bombSpawnTimer    int
-// 	isComplete        bool
-// 	background        *ebiten.Image
-// 	groundHeight      float64
-// 	isGameOver        bool
-// }
-
-// func NewLevel1() *Level1 {
-// 	player := ebiten.NewImage(32, 32)
-// 	player.Fill(color.RGBA{R: 50, G: 205, B: 50, A: 255})
-
-// 	bg := ebiten.NewImage(screenWidth, screenHeight)
-// 	bg.Fill(color.RGBA{R: 135, G: 206, B: 235, A: 255})
-
-// 	groundH := 40.0
-
-// 	l1 := &Level1{
-// 		playerX:           screenWidth / 2,
-// 		playerY:           screenHeight - groundH - 32,
-// 		playerSpeed:       4.0,
-// 		playerHealth:      3,
-// 		playerImg:         player,
-// 		powerupsCollected: 0,
-// 		bombSpawnTimer:    120, // Spawn a bomb every 2 seconds
-// 		isComplete:        false,
-// 		isGameOver:        false,
-// 		background:        bg,
-// 		groundHeight:      groundH,
-// 	}
-
-// 	// Create 5 powerups at random positions
-// 	powerupImg := ebiten.NewImage(16, 16)
-// 	powerupImg.Fill(color.RGBA{R: 255, G: 255, B: 0, A: 255})
-// 	for i := 0; i < 5; i++ {
-// 		pu := &Powerup{
-// 			X:   rand.Float64() * (screenWidth - 16),
-// 			Y:   screenHeight - groundH - 16,
-// 			Img: powerupImg,
-// 		}
-// 		l1.powerups = append(l1.powerups, pu)
-// 	}
-
-// 	return l1
-// }
-
-// func (l *Level1) Reset() {
-// 	// By calling NewLevel1(), we get a fresh state
-// 	newState := NewLevel1()
-// 	*l = *newState
-// }
-
-// func (l *Level1) Update() {
-// 	if l.isGameOver {
-// 		// You could add a timer or key press to restart
-// 		l.Reset()
-// 		return
-// 	}
-
-// 	// Player Movement
-// 	if inpututil.IsKeyJustPressed(ebiten.KeyArrowLeft) || ebiten.IsKeyPressed(ebiten.KeyA) {
-// 		l.playerX -= l.playerSpeed
-// 	}
-// 	if inpututil.IsKeyJustPressed(ebiten.KeyArrowRight) || ebiten.IsKeyPressed(ebiten.KeyD) {
-// 		l.playerX += l.playerSpeed
-// 	}
-// 	if l.playerX < 0 {
-// 		l.playerX = 0
-// 	}
-// 	if l.playerX > screenWidth-32 {
-// 		l.playerX = screenWidth - 32
-// 	}
-
-// 	// Bomb Spawning
-// 	l.bombSpawnTimer--
-// 	if l.bombSpawnTimer <= 0 {
-// 		bombImg := ebiten.NewImage(10, 20)
-// 		bombImg.Fill(color.Black)
-// 		newBomb := &Bomb{
-// 			X:   rand.Float64() * (screenWidth - 10),
-// 			Y:   -20, // Start just above the screen
-// 			Img: bombImg,
-// 		}
-// 		l.bombs = append(l.bombs, newBomb)
-// 		l.bombSpawnTimer = rand.Intn(90) + 30 // Spawn next bomb in 0.5 to 1.5 seconds
-// 	}
-
-// 	playerRect := ebiten.NewImage(32, 32).Bounds()
-// 	playerRect = playerRect.Add(image.Pt(int(l.playerX), int(l.playerY)))
-
-// 	// Update Bombs and Check Collision
-// 	for i := len(l.bombs) - 1; i >= 0; i-- {
-// 		bomb := l.bombs[i]
-// 		bomb.Y += 3.0 // Bomb fall speed
-
-// 		// Remove bomb if it's off-screen
-// 		if bomb.Y > screenHeight {
-// 			l.bombs = append(l.bombs[:i], l.bombs[i+1:]...)
-// 			continue
-// 		}
-
-// 		bombRect := bomb.Img.Bounds().Add(image.Pt(int(bomb.X), int(bomb.Y)))
-// 		if playerRect.Overlaps(bombRect) {
-// 			l.playerHealth--
-// 			l.bombs = append(l.bombs[:i], l.bombs[i+1:]...) // Remove bomb on hit
-// 			if l.playerHealth <= 0 {
-// 				l.isGameOver = true
-// 			}
-// 		}
-// 	}
-
-// 	// Check Powerup Collection
-// 	for i := len(l.powerups) - 1; i >= 0; i-- {
-// 		powerup := l.powerups[i]
-// 		powerupRect := powerup.Img.Bounds().Add(image.Pt(int(powerup.X), int(powerup.Y)))
-// 		if playerRect.Overlaps(powerupRect) {
-// 			l.powerupsCollected++
-// 			l.powerups = append(l.powerups[:i], l.powerups[i+1:]...) // Remove collected powerup
-// 		}
-// 	}
-
-// 	// Win Condition
-// 	if l.powerupsCollected >= 5 {
-// 		l.isComplete = true
-// 	}
-// }
-
-// func (l *Level1) Draw(screen *ebiten.Image) {
-// 	screen.DrawImage(l.background, nil)
-
-// 	ground := ebiten.NewImage(screenWidth, int(l.groundHeight))
-// 	ground.Fill(color.RGBA{R: 139, G: 69, B: 19, A: 255})
-// 	groundOps := &ebiten.DrawImageOptions{}
-// 	groundOps.GeoM.Translate(0, screenHeight-l.groundHeight)
-// 	screen.DrawImage(ground, groundOps)
-
-// 	playerOps := &ebiten.DrawImageOptions{}
-// 	playerOps.GeoM.Translate(l.playerX, l.playerY)
-// 	screen.DrawImage(l.playerImg, playerOps)
-
-// 	for _, p := range l.powerups {
-// 		puOps := &ebiten.DrawImageOptions{}
-// 		puOps.GeoM.Translate(p.X, p.Y)
-// 		screen.DrawImage(p.Img, puOps)
-// 	}
-
-// 	for _, b := range l.bombs {
-// 		bombOps := &ebiten.DrawImageOptions{}
-// 		bombOps.GeoM.Translate(b.X, b.Y)
-// 		screen.DrawImage(b.Img, bombOps)
-// 	}
-
-// 	// Draw HUD
-// 	healthText := fmt.Sprintf("Health: %d", l.playerHealth)
-// 	powerupText := fmt.Sprintf("Powerups: %d/5", l.powerupsCollected)
-// 	ebitenutil.DebugPrint(screen, healthText+"\n"+powerupText)
-// }
-
-// func (l *Level1) IsDone() bool {
-// 	return l.isComplete
-// }
-
 package main
 
 import (
@@ -199,10 +6,16 @@ import (
 	"image/color"
 	"log"
 	"math/rand"
+	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
+
+// Seed the random number generator once
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 // Helper function to load images
 func loadImage(path string) *ebiten.Image {
@@ -212,6 +25,17 @@ func loadImage(path string) *ebiten.Image {
 	}
 	return img
 }
+
+const (
+	// Character is now 64x64 pixels
+	playerLogicWidth   = 80
+	playerLogicHeight  = 80
+	powerupLogicWidth  = 25
+	powerupLogicHeight = 25
+	// Bomb is now 20x40 pixels
+	bombLogicWidth  = 40
+	bombLogicHeight = 60
+)
 
 type Powerup struct {
 	X, Y float64
@@ -234,7 +58,6 @@ type Level1 struct {
 	isGameOver        bool
 	groundHeight      float64
 
-	// Image assets
 	backgroundImg *ebiten.Image
 	playerImg     *ebiten.Image
 	bombImg       *ebiten.Image
@@ -242,42 +65,36 @@ type Level1 struct {
 }
 
 func NewLevel1() *Level1 {
-	// Load all assets from the "assets" folder
 	bgImg := loadImage("assets/level1_theme.png")
 	playerImg := loadImage("assets/level1_character.png")
 	bombImg := loadImage("assets/level1_bomb.png")
 	powerupImg := loadImage("assets/level1_powerup.png")
 
 	groundH := 40.0
-	playerW, playerH := playerImg.Size()
 
 	l1 := &Level1{
 		playerX:           screenWidth / 2,
-		playerY:           screenHeight - groundH - float64(playerH),
+		playerY:           screenHeight - groundH - playerLogicHeight,
 		playerSpeed:       4.0,
 		playerHealth:      3,
 		powerupsCollected: 0,
-		bombSpawnTimer:    120, // Spawn a bomb every 2 seconds
+		bombSpawnTimer:    120,
 		isComplete:        false,
 		isGameOver:        false,
 		groundHeight:      groundH,
-		// Assign loaded images
-		backgroundImg: bgImg,
-		playerImg:     playerImg,
-		bombImg:       bombImg,
-		powerupImg:    powerupImg,
+		backgroundImg:     bgImg,
+		playerImg:         playerImg,
+		bombImg:           bombImg,
+		powerupImg:        powerupImg,
 	}
 
-	// Create 5 powerups at random positions
-	powerupW, _ := l1.powerupImg.Size()
 	for i := 0; i < 5; i++ {
 		pu := &Powerup{
-			X: rand.Float64() * (screenWidth - float64(powerupW)),
-			Y: screenHeight - groundH - float64(l1.powerupImg.Bounds().Dy()),
+			X: rand.Float64() * (screenWidth - powerupLogicWidth),
+			Y: screenHeight - groundH - powerupLogicHeight,
 		}
 		l1.powerups = append(l1.powerups, pu)
 	}
-
 	return l1
 }
 
@@ -292,9 +109,6 @@ func (l *Level1) Update() {
 		return
 	}
 
-	playerW, _ := l.playerImg.Size()
-
-	// Player Movement
 	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) || ebiten.IsKeyPressed(ebiten.KeyA) {
 		l.playerX -= l.playerSpeed
 	}
@@ -304,94 +118,87 @@ func (l *Level1) Update() {
 	if l.playerX < 0 {
 		l.playerX = 0
 	}
-	if l.playerX > screenWidth-float64(playerW) {
-		l.playerX = screenWidth - float64(playerW)
+	if l.playerX > screenWidth-playerLogicWidth {
+		l.playerX = screenWidth - playerLogicWidth
 	}
 
-	// Bomb Spawning
 	l.bombSpawnTimer--
 	if l.bombSpawnTimer <= 0 {
-		bombW, _ := l.bombImg.Size()
 		newBomb := &Bomb{
-			X: rand.Float64() * (screenWidth - float64(bombW)),
-			Y: -float64(l.bombImg.Bounds().Dy()), // Start just above the screen
+			X: rand.Float64() * (screenWidth - bombLogicWidth),
+			Y: -bombLogicHeight,
 		}
 		l.bombs = append(l.bombs, newBomb)
-		l.bombSpawnTimer = rand.Intn(90) + 30 // Spawn next bomb in 0.5 to 1.5 seconds
+		l.bombSpawnTimer = rand.Intn(90) + 30
 	}
 
-	playerRect := l.playerImg.Bounds().Add(image.Pt(int(l.playerX), int(l.playerY)))
+	playerRect := image.Rect(int(l.playerX), int(l.playerY), int(l.playerX)+playerLogicWidth, int(l.playerY)+playerLogicHeight)
 
-	// Update Bombs and Check Collision
 	for i := len(l.bombs) - 1; i >= 0; i-- {
 		bomb := l.bombs[i]
-		bomb.Y += 3.0 // Bomb fall speed
-
+		bomb.Y += 3.0
 		if bomb.Y > screenHeight {
 			l.bombs = append(l.bombs[:i], l.bombs[i+1:]...)
 			continue
 		}
-
-		bombRect := l.bombImg.Bounds().Add(image.Pt(int(bomb.X), int(bomb.Y)))
+		bombRect := image.Rect(int(bomb.X), int(bomb.Y), int(bomb.X)+bombLogicWidth, int(bomb.Y)+bombLogicHeight)
 		if playerRect.Overlaps(bombRect) {
 			l.playerHealth--
-			l.bombs = append(l.bombs[:i], l.bombs[i+1:]...) // Remove bomb on hit
+			l.bombs = append(l.bombs[:i], l.bombs[i+1:]...)
 			if l.playerHealth <= 0 {
 				l.isGameOver = true
 			}
 		}
 	}
 
-	// Check Powerup Collection
 	for i := len(l.powerups) - 1; i >= 0; i-- {
 		powerup := l.powerups[i]
-		powerupRect := l.powerupImg.Bounds().Add(image.Pt(int(powerup.X), int(powerup.Y)))
+		powerupRect := image.Rect(int(powerup.X), int(powerup.Y), int(powerup.X)+powerupLogicWidth, int(powerup.Y)+powerupLogicHeight)
 		if playerRect.Overlaps(powerupRect) {
 			l.powerupsCollected++
-			l.powerups = append(l.powerups[:i], l.powerups[i+1:]...) // Remove collected powerup
+			l.powerups = append(l.powerups[:i], l.powerups[i+1:]...)
 		}
 	}
 
-	// Win Condition
 	if l.powerupsCollected >= 5 {
 		l.isComplete = true
 	}
 }
 
 func (l *Level1) Draw(screen *ebiten.Image) {
-	// Draw Background
 	bgOpts := &ebiten.DrawImageOptions{}
 	bgW, bgH := l.backgroundImg.Size()
 	bgOpts.GeoM.Scale(screenWidth/float64(bgW), screenHeight/float64(bgH))
 	screen.DrawImage(l.backgroundImg, bgOpts)
 
-	// Draw Ground (a simple colored bar, can be replaced with an asset)
 	ground := ebiten.NewImage(screenWidth, int(l.groundHeight))
 	ground.Fill(color.RGBA{R: 139, G: 69, B: 19, A: 255})
 	groundOps := &ebiten.DrawImageOptions{}
 	groundOps.GeoM.Translate(0, screenHeight-l.groundHeight)
 	screen.DrawImage(ground, groundOps)
 
-	// Draw Player
 	playerOps := &ebiten.DrawImageOptions{}
+	pW, pH := l.playerImg.Size()
+	playerOps.GeoM.Scale(playerLogicWidth/float64(pW), playerLogicHeight/float64(pH))
 	playerOps.GeoM.Translate(l.playerX, l.playerY)
 	screen.DrawImage(l.playerImg, playerOps)
 
-	// Draw Powerups
 	for _, p := range l.powerups {
 		puOps := &ebiten.DrawImageOptions{}
+		puW, puH := l.powerupImg.Size()
+		puOps.GeoM.Scale(powerupLogicWidth/float64(puW), powerupLogicHeight/float64(puH))
 		puOps.GeoM.Translate(p.X, p.Y)
 		screen.DrawImage(l.powerupImg, puOps)
 	}
 
-	// Draw Bombs
 	for _, b := range l.bombs {
 		bombOps := &ebiten.DrawImageOptions{}
+		bW, bH := l.bombImg.Size()
+		bombOps.GeoM.Scale(bombLogicWidth/float64(bW), bombLogicHeight/float64(bH))
 		bombOps.GeoM.Translate(b.X, b.Y)
 		screen.DrawImage(l.bombImg, bombOps)
 	}
 
-	// Draw HUD
 	healthText := fmt.Sprintf("Health: %d", l.playerHealth)
 	powerupText := fmt.Sprintf("Powerups: %d/5", l.powerupsCollected)
 	ebitenutil.DebugPrint(screen, healthText+"\n"+powerupText)
